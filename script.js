1c9967d924f49a1a48a8ee30dec4e25929cc67d0
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2022-11-09 19:04:12 +0300

    Add genres, ratings, and descriprions

--- script.js
+++ script.js
@@ -38,9 +38,23 @@ const fetchMovie = async (movieId) => {
   return res.json();
 };
 
+const fetchGenreName = async (genreId) => {
+  const url = constructUrl(`genre/movie/list`);
+  const res = await fetch(url);
+  const data = await res.json();
+  const genre = data.genres.find((genre) => genre.id === genreId);
+  return genre.name;
+};
+
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
-  movies.map((movie) => {
+
+  movies.map(async (movie) => {
+    const genreName = movie.genre_ids.map(async (genreId) => {
+      return await fetchGenreName(genreId);
+    });
+    const genreNames = await Promise.all(genreName);
+
     const movieContainer = document.createElement("div");
     movieContainer.classList.add(
       "movie", "flex", "flex-col", "justify-center", "items-center",
@@ -48,19 +62,38 @@ const renderMovies = (movies) => {
       "cursor-pointer", "transition", "duration-500", "ease-in-out", "transform",
       "dark:bg-neutral-700", "dark:hover:bg-neutral-600",
       "hover:shadow-2xl", "hover:bg-neutral-400", "hover:-translate-y-1", "hover:scale-110",
-
     );
     movieContainer.innerHTML = `
       <img src="${BACKDROP_BASE_URL + movie.backdrop_path}"
-        alt="${movie.title} poster">
-      <h3 class="movie-title my-2 text-lg font-bold text-opacity-75
-        hover:text-opacity-100 transition duration-500 ease-in-out cursor-pointer">
+        alt="${movie.title} poster" width="780" height="439">
+      <h3 class="movie-title my-2 text-lg font-bold">
         ${movie.title}
       </h3>
+      <p class="genre-names text-sm">
+        Genres: ${genreNames.join(", ")}
+      </p>
+      <p class="movie-rating text-sm pb-4">
+        Average vote: ${movie.vote_average}
+      </p>
+      <p class="description hidden absolute bottom-0 p-4 text-sm tracking-wide
+          text-center bg-neutral-200 dark:bg-neutral-700 animate-fade-in-down">
+        ${movie.overview}
+      </p>
       `;
     movieContainer.addEventListener("click", () => {
       movieDetails(movie);
     });
+
+    movieContainer.addEventListener("mouseover", () => {
+      const description = movieContainer.querySelector(".description");
+      description.classList.remove("hidden");
+    });
+
+    movieContainer.addEventListener("mouseout", () => {
+      const description = movieContainer.querySelector(".description");
+      description.classList.add("hidden");
+    });
+
     CONTAINER.appendChild(movieContainer);
   });
 };
