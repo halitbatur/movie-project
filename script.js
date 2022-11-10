805c7af8dbc9017fd18772922f43ba866561c4a9
Hala-AlArid <113375497+Hala-AlArid@users.noreply.github.com>
2022-11-10 23:25:03 +0200

    Update script.js

--- script.js
+++ script.js
@@ -4,10 +4,17 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.querySelector(".container");
+const ACTORSLINK = document.querySelector(".Actors-link");
+const HOMELINK = document.querySelector(".Home-link");
+const MOVIESLINK = document.querySelector(".movies-link");
+const dropdown = document.querySelector(".dropdown-content")
+
+//fetching movies
 
 // Don't touch this function please
 const autorun = async () => {
   const movies = await fetchMovies();
+  console.log(movies.results)
   renderMovies(movies.results);
 };
 
@@ -68,6 +75,7 @@ const renderMovie = (movie) => {
             <p id="movie-release-date"><b>Release Date:</b> ${
               movie.release_date
             }</p>
+            <p id="movie-genre"><b>Genre:</b> ${movie.genres[0].name}</p>
             <p id="movie-runtime"><b>Runtime:</b> ${movie.runtime} Minutes</p>
             <h3>Overview:</h3>
             <p id="movie-overview">${movie.overview}</p>
@@ -78,4 +86,106 @@ const renderMovie = (movie) => {
     </div>`;
 };
 
+
 document.addEventListener("DOMContentLoaded", autorun);
+
+// Fetching Actors
+
+const runActors = async () => {
+  const actors = await fetchActors();
+  console.log(actors);
+  renderActors(actors.results);
+};
+
+
+const constructUrlActor = (path) => {
+  return `${TMDB_BASE_URL}/${path}?api_key=${atob(
+    "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
+  )}`;
+};
+
+const actorDetails = async (actor) => {
+    const actorRes = await fetchActor(actor.id);
+    renderActor(actorRes);
+  };
+
+const fetchActors = async () => {
+  const url = constructUrlActor(`person/popular`);
+  const res = await fetch(url);
+  return res.json();
+};
+
+
+const fetchActor = async (actorId) => {
+    const url = constructUrl(`person/${actorId}`);
+    const res = await fetch(url);
+    return res.json();
+  };
+  
+
+  const renderActors = (actors) => {
+    actors.map((actor) => {
+      const actorDiv = document.createElement("div");
+      actorDiv.innerHTML = `
+          <img src="${PROFILE_BASE_URL + actor.profile_path}" alt="${
+        actor.name
+      } poster">
+          <h3>${actor.name}</h3>`;
+      actorDiv.addEventListener("click", () => {
+        actorDetails(actor);
+      });
+      CONTAINER.appendChild(actorDiv);
+    });
+  };
+  
+  const renderActor = (actor) => {
+    CONTAINER.innerHTML = `
+      <div class="row">
+          <div class="col-md-4">
+               <img id="actor-backdrop" src=${PROFILE_BASE_URL + actor.profile_path}>
+          </div>
+          <div class="col-md-8">
+              <h2 id="actor-name">${actor.name}</h2>
+              </div>
+      </div>`;
+  };
+  
+  if(ACTORSLINK){
+    ACTORSLINK.addEventListener("click", function (e) {
+      e.preventDefault();
+      CONTAINER.replaceChildren();
+      runActors();
+    });
+  }
+
+//loading genres
+
+const runGenres = async () => {
+  const genres = await fetchGenres();
+  console.log(genres.genres);
+  renderGenres(genres.genres);
+};
+
+const constructUrlGenre = (path) => {
+  return `${TMDB_BASE_URL}/${path}?api_key=${atob(
+    "NTQyMDAzOTE4NzY5ZGY1MDA4M2ExM2M0MTViYmM2MDI="
+  )}`;
+};
+
+const fetchGenres = async () => {
+  const url = constructUrlGenre(`genre/movie/list`);
+  const res = await fetch(url);
+  return res.json();
+};
+
+const renderGenres = (genres) => {
+  genres.map((genre) => {
+    const genresDiv = document.createElement("a");
+    genresDiv.classList.add("genre-link");
+    genresDiv.innerHTML = `${genre.name}`;
+    dropdown.appendChild(genresDiv);
+  });
+
+};
+
+document.addEventListener("DOMContentLoaded", runGenres);
