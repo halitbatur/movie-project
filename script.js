ec3b9c7a3a2e393e14477e1a6453743ff30dc4bd
Danah <osta.danah@gmail.com>
2022-11-12 22:43:35 +0200

    fetchec actors - rendered actors + styled actors list page - rendered actor + fetched movie credits for single actor page

--- script.js
+++ script.js
@@ -4,17 +4,21 @@ const TMDB_BASE_URL = "https://api.themoviedb.org/3";
 const PROFILE_BASE_URL = "http://image.tmdb.org/t/p/w185";
 const BACKDROP_BASE_URL = "http://image.tmdb.org/t/p/w780";
 const CONTAINER = document.createElement("div");
+const CONTAINER2 = document.createElement("div");
 CONTAINER.classList.add("container");
 const mainContainer = document.querySelector(".mainContainer");
 mainContainer.appendChild(CONTAINER)
 
+//For Movies & Actors
 // Don't touch this function please
 const autorun = async () => {
   const movies = await fetchMovies();
+  const actors = await fetchActors();
   handleHeaderSection();
-
   renderMovies(movies.results);
+  renderActors(actors.results);
 };
+
 
 // Don't touch this function please
 const constructUrl = (path) => {
@@ -23,6 +27,7 @@ const constructUrl = (path) => {
   )}`;
 };
 
+// MovieDetails Function
 // You may need to add to this function, definitely don't delete it.
   const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
@@ -31,10 +36,27 @@ const constructUrl = (path) => {
   renderMovie(movieRes, credits, similarMovies);
 };
 
+// ActorDetails Function
+// You may need to add to this function, definitely don't delete it.
+const actorDetails = async (actor) => {
+  const actorRes = await fetchActor(actor.id);
+  const movieCredits = await fetchMovieCredits(actor.id)
+  renderActor(actorRes, movieCredits);
+};
+
+// fetchMovies Function
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
   const fetchMovies = async () => {
   const url = constructUrl(`movie/now_playing`);
   const res = await fetch(url);
+  return res.json();
+}; 
+
+// fetchActors Function
+// This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
+const fetchActors = async () => {
+  const url2 = constructUrl(`person/popular`);
+  const res = await fetch(url2);
   return res.json();
 }; 
 
@@ -46,10 +68,27 @@ const constructUrl = (path) => {
 // 5) Get Genre
 */
 
+//fetchMovie Function
 // Don't touch this function please. This function is to fetch one movie.
   const fetchMovie = async (movieId) => {
   const url = constructUrl(`movie/${movieId}`);
   const res = await fetch(url);
+  return res.json();
+};
+
+//fetchActor Function
+// Don't touch this function please. This function is to fetch one actor.
+const fetchActor = async (actorId) => {
+  const url2 = constructUrl(`person/${actorId}`);
+  const res = await fetch(url2);
+  return res.json();
+};
+
+//fetchMovieCredits Function
+// Don't touch this function please. This function is to fetch movie credits for each actor.
+const fetchMovieCredits = async (actorId) => {
+  const url2 = constructUrl(`person/${actorId}/movie_credits`);
+  const res = await fetch(url2);
   return res.json();
 };
 
@@ -81,8 +120,7 @@ function handleHeaderSection() {
 }
    const renderMovies = (movies) => {
   // handleSlider(movies);
-  console.log(movies);
-   //console.log(movies)
+  //console.log(movies);
    const newDiv = document.createElement('div')
    newDiv.classList.add("grid","grid-cols-1","gap-6","md:grid-cols-2","lg:grid-cols-3","lg:px-20")
    movies.map((movie) => {
@@ -138,10 +176,92 @@ function handleHeaderSection() {
    });
 };
 
+// renderActors Function
+const renderActors = (actors) => {
+       console.log(actors)
+      const newDiv2 = document.createElement('div')
+      newDiv2.classList.add("grid","grid-cols-1","gap-6","md:grid-cols-2","lg:grid-cols-3","lg:px-20")
+      actors.map( (actor) => {
+
+          const actorDiv = document.createElement("div");
+          actorDiv.classList.add('actorContainer')
+          actorDiv.innerHTML = `
+           <div class= "w-80 mx-auto">
+             <div class="image-container relative w-full border-8 border-black">
+               <img src="${PROFILE_BASE_URL  + actor.profile_path}" alt="${actor.name}" poster class=" home-movie-image opacity-100 block w-full h-auto transition ease-in duration-500">
+               <div class="middle transition ease-in duration-500 opacity-0 absolute -translate-y-2/4 -translate-x-2/4 text-justify w-72">
+               <div class="textonimage bg-black text-white text-xs w-full p-3"><b>Id:</b> ${actor.id} <br><b>Genres:</b>??  <br><b>Description:</b> ??</div>
+               </div>
+             </div>
+             <h1 class= "text-center text-2xl md:text-3xl font-bold font-mono bg-black text-white h-20 md:h-28 lg:h-36 xl:h-28 m-auto p-3">${actor.name}</h1>
+          </div> `
+          actorDiv.addEventListener("click", () => {
+            actorDetails(actor);
+          });
+
+       newDiv2.appendChild(actorDiv)
+       CONTAINER.appendChild(newDiv2);
+       mainContainer.appendChild(CONTAINER);
+      });
+};
+
+//renderActor Function
+const renderActor = (actor, movieCredits) => {
+   console.log(actor, movieCredits)
+
+   CONTAINER.innerHTML = `
+    <div class="row bg-white text-black mx-auto w-full">
+        <div class="col-md-4">
+             <img id="actor-backdrop" src=${PROFILE_BASE_URL + actor.profile_path}>
+        </div>
+
+        <div class="col-md-8 bg-white text-black w-full">
+            <h2 id="actor-name"><b>Name:</b> ${actor.name}</h2>
+            <p id="actor-gender"><b>Gender:</b> ${actor.gender}</p>
+            <p id="actor-popularity"><b>Popularity:</b> ${actor.popularity} </p>
+            <p id="actor-birthday"><b>Birthday:</b> ${actor.birthday} </p>
+            <p id="actor-deathday"><b>Deathday:</b> ${actor.deathday} </p>
+            <h3 class="pt-6"><b>Biography:</b></h3>
+            <p id="actor-biography">${actor.biography}</p>
+        </div>
+        
+        <h3>List of Movies the Actor Participated in:</h3>
+        <ul id="list-of-movies-actor-participated-in" class="list-unstyled" >
+             <li> 
+                <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[0].id}"> ${movieCredits.cast[0].title}</a>
+             </li>
+                
+             <li> 
+                <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[1].id}"> ${movieCredits.cast[1].title}</a>
+             </li>
+                
+             <li> 
+                <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[2].id}"> ${movieCredits.cast[2].title}</a>
+             </li>
+                
+             <li> 
+                <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[3].id}"> ${movieCredits.cast[3].title}</a>
+             </li>
+                
+             <li> 
+                <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[4].id}"> ${movieCredits.cast[4].title}</a>
+             </li>
+
+             <li> 
+                <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[5].id}"> ${movieCredits.cast[5].title}</a>
+             </li>
+
+             <li> 
+               <a href="${TMDB_BASE_URL}/movie/${movieCredits.cast[6].id}"> ${movieCredits.cast[6].title}</a>
+              </li>
+         </ul> 
+    </div>`;
+};
+
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovie = (movie, credits, similarMovies) => {
-  console.log({movie, credits, similarMovies})
-    document.querySelector(".headerSection-container").remove();
+  //console.log({movie, credits, similarMovies})
+   // document.querySelector(".headerSection-container").remove();
   CONTAINER.innerHTML = `
     <div class="row bg-black text-white mx-auto w-full">
         <div class="col-md-4">
@@ -185,7 +305,7 @@ const renderMovie = (movie, credits, similarMovies) => {
         </div>
 
         <div class="pt-20">
-        <p id="director-name"> <b>Director's Name</b>  ${credits.crew[15].name} </p>
+        <p id="director-name"> <b>Director's Name</b> ??</p>
         </div>
 
         <div class="pt-20">
