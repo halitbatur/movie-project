cdff62860eaa163d7bb15c401de219683a24efb4
Nisreen Hamzah <nisreenhamzah138@gmail.com>
2022-11-13 01:34:32 +0200

    worked on actors

--- script.js
+++ script.js
@@ -29,6 +29,8 @@ const constructUrl = (path) => {
 const movieDetails = async (movie) => {
   const movieRes = await fetchMovie(movie.id);
   renderMovie(movieRes);
+  const movieRes2 = await fetchActorsForMovie(movie.id)
+  renderActorsofMovie(movieRes2);
 };
 
 // This function is to fetch movies. You may need to add it or change some part in it in order to apply some of the features.
@@ -49,11 +51,13 @@ const fetchMovie = async (movieId) => {
 const renderMovies = (movies) => {
   movies.map((movie) => {
     const movieDiv = document.createElement("div");
+    movieDiv.className = "card"
     movieDiv.innerHTML = `
         <img src="${BACKDROP_BASE_URL + movie.backdrop_path}" alt="${
       movie.title
     } poster">
-        <h3>${movie.title}</h3>`;
+        <h3>${movie.title}</h3>
+        <p class="img-description">${movie.title} <br> ${movie.vote_average}</p>`;
     movieDiv.addEventListener("click", () => {
       movieDetails(movie);
     });
@@ -84,6 +88,7 @@ const renderMovie = (movie) => {
             <h3>Actors:</h3>
             <ul id="actors" class="list-unstyled"></ul>
     </div>`;
+    runfetchActorsForMovie()
 };
 
 
@@ -107,6 +112,8 @@ const constructUrlActor = (path) => {
 const actorDetails = async (actor) => {
     const actorRes = await fetchActor(actor.id);
     renderActor(actorRes);
+    const actorRes2 = await fetchMoviesForActor(actor.id)
+    renderMoviesofActor(actorRes2)
   };
 
 const fetchActors = async () => {
@@ -124,8 +131,10 @@ const fetchActor = async (actorId) => {
   
 
   const renderActors = (actors) => {
+    console.log(actors)
     actors.map((actor) => {
       const actorDiv = document.createElement("div");
+      actorDiv.className = "actorDiv"
       actorDiv.innerHTML = `
           <img src="${PROFILE_BASE_URL + actor.profile_path}" alt="${
         actor.name
@@ -138,23 +147,97 @@ const fetchActor = async (actorId) => {
     });
   };
   
+  
+//// NISREEN STARTED HERE
+function checkGender(num){
+  if(num == 1){
+      return "Female"
+  } else if(num == 2){
+    return "Male"
+  }
+}
+
   const renderActor = (actor) => {
     CONTAINER.innerHTML = `
-      <div class="row">
-          <div class="col-md-4">
-               <img id="actor-backdrop" src=${PROFILE_BASE_URL + actor.profile_path}>
-          </div>
-          <div class="col-md-8">
-              <h2 id="actor-name">${actor.name}</h2>
-              </div>
-      </div>`;
+    <div class="row">
+        <div class="col-md-4">
+             <img id="actor-backdrop" src=${
+               PROFILE_BASE_URL + actor.profile_path}>
+        </div>
+        <div class="col-md-8">
+            <h2 id="actor-name">${actor.name} (${checkGender(actor.gender)})</h2>
+            <p id="actor-birth-date"><b>BirthDate:</b> ${actor.birthday}</p>
+            <h3>Biography:</h3>
+            <p id="actor-overview">${actor.biography}</p>
+     </div>
+     </div>
+     </div><br><br><br><br><br>`
+    
+    if(actor.deathday != null){
+      CONTAINER.innerHTML += `<p id="actor-death-date"><b>DeathDay:</b> ${actor.deathday} </p>`
+    }
+   };
+
+  //FETCH ALL MOVIES OF A CERTAIN ACTOR
+  const fetchMoviesForActor = async (actorId) => {
+    const url = constructUrl(`person/${actorId}/movie_credits`);
+    const res = await fetch(url);
+    return res.json();
+  }
+
+  const runActormovieDetails = async (actor) => {
+    const ActormovieRes = await fetchMoviesForActor(actor.id);
+    renderMoviesofActor(ActormovieRes);
   };
-  
+
+  const renderMoviesofActor = (actor) => {
+    actor.cast.map((cast) => {
+      const actormovieDiv = document.createElement("div");
+      actormovieDiv.className = "ActorMovies"
+     if(cast.backdrop_path != null){
+     actormovieDiv.innerHTML = `
+          <img src="${BACKDROP_BASE_URL + cast.backdrop_path}" alt="Movie poster">
+          <h3>${cast.title}</h3>`;
+     }
+      CONTAINER.appendChild(actormovieDiv);
+    });
+   };
+   //FETCH ALL MOVIES OF A CERTAIN ACTOR END HERE
+
+  //FETCH ACTORS OF A CERTAIN MOVIE
+  const fetchActorsForMovie= async (movieId) => {
+    const url = constructUrl(`movie/${movieId}/credits`);
+    const res = await fetch(url);
+    return res.json();
+  }
+
+  const runfetchActorsForMovie = async (movie) => {
+    const ActorsOfmovieRes = await fetchActorsForMovie(movie.id);
+    renderActorsofMovie(ActorsOfmovieRes);
+  };
+
+  const renderActorsofMovie = (movie) => {
+    movie.cast.map((cast) => {
+      const actormovieDiv = document.createElement("div");
+      actormovieDiv.className = "ActorMovies"
+     if(cast.profile_path != null){
+     actormovieDiv.innerHTML = `
+          <img src="${PROFILE_BASE_URL + cast.profile_path}" alt="Actor poster">
+          <h3>${cast.name}</h3>`;
+     }
+      CONTAINER.appendChild(actormovieDiv);
+    });
+   };
+  //FETCH ACTORS OF CERTAIN MOVIE ENDS HERE
+
+  // NISREEN FINISHED HERE
+
   if(ACTORSLINK){
     ACTORSLINK.addEventListener("click", function (e) {
       e.preventDefault();
       CONTAINER.replaceChildren();
       runActors();
+      
     });
   }
 
