2ad66f55876303c570a89c18f8572ef33f903616
Aland Abdullah <32212842+alandio@users.noreply.github.com>
2022-11-14 22:23:28 +0000

    Fix eslint error

--- components/actor.js
+++ components/actor.js
@@ -1,7 +1,5 @@
-/* eslint-disable */
-
-import { CONTAINER, BACKDROP_BASE_URL } from "../utils/constants.js";
-import constructUrl from "../utils/urls.js";
+import { CONTAINER, BACKDROP_BASE_URL } from '../utils/constants.js';
+import constructUrl from '../utils/urls.js';
 
 const fetchMoviesParticipated = async (actorId) => {
   const url = constructUrl(`person/${actorId}/movie_credits`);
@@ -23,7 +21,7 @@ const renderActor = async (actor) => {
     </div>
 
     <!-- Actor details -->
-    <div class="flex flex-col gap-4 ml-16 md:w-1/2 justify-center basis-2\/3 ">
+    <div class="flex flex-col gap-4 ml-16 md:w-1/2 justify-center basis-2/3 ">
         <p id="actor-name" class="text-base"><b>Name: </b>  ${actor.name} </p>
         <p id="actor-birthday" class="text-base">
           <b>Birthday: </b> ${actor.birthday}
@@ -33,7 +31,7 @@ const renderActor = async (actor) => {
           <b>place_of_birth: </b> ${actor.place_of_birth}
         </p>
         <p id="movie-language" class="text-base">
-          <b>Gender: </b>${actor.gender === 1 ? "Male" : "Female"}
+          <b>Gender: </b>${actor.gender === 1 ? 'Male' : 'Female'}
         </p>
         <p id="movie-language" class="text-base">
           <b>Popularity: </b>${actor.popularity}
@@ -42,41 +40,38 @@ const renderActor = async (actor) => {
           <b>Death Day: </b>${actor.deathday}
         </p>
         <p id="actor-biography" class="text-base">
-          <b>biography: </b> ${
-            actor.biography.length > 500
-              ? actor.biography.slice(0, 500) +
-                " ... " +
-                "<a class='text-blue-700' href=#>READ MORE</a>"
-              : actor.biography
-          }
+          <b>biography: </b> ${actor.biography.length > 500
+    ? `${actor.biography.slice(0, 500)} ...
+        <a class='text-blue-700' href=#>READ MORE</a>
+      `
+    : actor.biography
+}
         </p>
 
         <!-- participated in Movies -->
 
           <h3 class="text-2xl font-bold">Movies</h3>
         <div id="participated-movies" class="flex flex-wrap w-full justify-center gap-4">
-          ${movies
-            .slice(0, 6)
-            .map((movie) => {
-              return `
-              <div data-id="${movie.id}" class="participated-movie flex flex-col
-                basis-40 justify-center items-center rounded overflow-hidden
-                bg-neutral-200 cursor-pointer transition duration-500 ease-in-out
-                transform dark:bg-neutral-700 dark:hover:bg-neutral-600
-                hover:shadow-2xl hover:bg-neutral-300 hover:-translate-y-1 hover:scale-110">
-                <img src="${BACKDROP_BASE_URL + movie.poster_path}"
-                  alt="${movie.title} poster" width="700" height="439">
-                <h3 class="movie-title my-2 text-base font-bold text-center">
-                  ${movie.title}
-                </h3>
-                <p class="movie-rating text-sm pb-4">
-                  Average vote: ${movie.vote_average}
-                </p>
+          ${movies.slice(0, 6).map((movie) => `
+            <div data-id="${movie.id}" class="participated-movie flex flex-col
+              basis-40 justify-center items-center rounded overflow-hidden
+              bg-neutral-200 cursor-pointer transition duration-500 ease-in-out
+              transform dark:bg-neutral-700 dark:hover:bg-neutral-600
+              hover:shadow-2xl hover:bg-neutral-300 hover:-translate-y-1 hover:scale-110">
 
-              </div>
-              `;
-            })
-            .join("")}
+              <img src="${BACKDROP_BASE_URL + movie.poster_path}"
+                alt="${movie.title} poster" width="700" height="439">
+
+              <h3 class="movie-title my-2 text-base font-bold text-center">
+                ${movie.title}
+              </h3>
+
+              <p class="movie-rating text-sm pb-4">
+                Average vote: ${movie.vote_average}
+              </p>
+
+            </div>
+            `).join('')}
         </div>
     </div>
 </div> <!-- End of container -->`;
--- components/home.js
+++ components/home.js
@@ -1,10 +1,8 @@
-/* eslint-disable */
-
-import { CONTAINER } from "../utils/constants.js";
-import renderMovies from "./movies.js";
+import { CONTAINER } from '../utils/constants.js';
+import renderMovies from './movies.js';
 
 const renderHome = (movies) => {
-  const navbar = document.querySelector(".navbar");
+  const navbar = document.querySelector('.navbar');
   navbar.innerHTML = `
 <select id = "filter" >
 <option> Filter Movies By </option>
@@ -12,48 +10,50 @@ const renderHome = (movies) => {
 <option> relase date </option>
 <option> top rated </option>
 </select> `;
-  const filter = document.getElementById("filter");
+  const filter = document.getElementById('filter');
 
   // when client clicked on select element
   // if default value is changed
-  filter.addEventListener("change", () => {
+  filter.addEventListener('change', () => {
     // if value switched by client
     switch (filter.value) {
-      case "popularity":
-        //do somthing with  , "add" value
-        movies.sort(function (a, b) {
-          let keyA = a.popularity,
-            keyB = b.popularity;
+      case 'popularity':
+        // do somthing with  , "add" value
+        movies.sort((a, b) => {
+          const keyA = a.popularity;
+          const keyB = b.popularity;
           // Compare the 2 dates
           if (keyA > keyB) return -1;
           if (keyA < keyB) return 1;
           return 0;
         });
         break; // then take break
-      case "relase date":
-        //do somthing with  , "remove" value
-        movies.sort(function (a, b) {
-          let keyA = new Date(a.release_date),
-            keyB = new Date(b.release_date);
+      case 'relase date':
+        // do somthing with  , "remove" value
+        movies.sort((a, b) => {
+          const keyA = new Date(a.release_date);
+          const keyB = new Date(b.release_date);
           // Compare the 2 dates
           if (keyA > keyB) return -1;
           if (keyA < keyB) return 1;
           return 0;
         });
         break; // then take break
-      case "top rated":
-        //do somthing with  , "add" value
-        movies.sort(function (a, b) {
-          let keyA = a.vote_average,
-            keyB = b.vote_average;
+      case 'top rated':
+        // do somthing with  , "add" value
+        movies.sort((a, b) => {
+          const keyA = a.vote_average;
+          const keyB = b.vote_average;
           // Compare the 2 dates
           if (keyA > keyB) return -1;
           if (keyA < keyB) return 1;
           return 0;
         });
         break; // then take break
+      default:
+        break;
     }
-    CONTAINER.innerHTML = "";
+    CONTAINER.innerHTML = '';
     renderMovies(movies);
   });
 };
