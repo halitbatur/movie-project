e6d1bfbc0f2dd65329b3fd4d8a573585efd89476
Ruba Alsoheil <rubasoheil@gmail.com>
2022-11-15 10:43:20 +0200

    search

--- script.js
+++ script.js
@@ -106,19 +106,17 @@ topRated.addEventListener("click", () => {
   FilterMovies("top_rated")
 })
 
-  const nowPlaying = document.querySelector("#now_playing");
-  nowPlaying.addEventListener("click", () => {
-    CONTAINER.innerHTML = "";
-    FilterMovies("now_playing")
-  })
+const nowPlaying = document.querySelector("#now_playing");
+nowPlaying.addEventListener("click", () => {
+  CONTAINER.innerHTML = "";
+  FilterMovies("now_playing")
+})
 
-  const upComing = document.querySelector("#upcoming");
-  upComing.addEventListener("click", () => {
-    CONTAINER.innerHTML = "";
-    FilterMovies("upcoming")
-  })
-  return popular, topRated, nowPlaying, upComing;
-}
+const upComing = document.querySelector("#upcoming");
+upComing.addEventListener("click", () => {
+  CONTAINER.innerHTML = "";
+  FilterMovies("upcoming")
+})
 // end of filter nav
 
 // rendering Home page
@@ -156,6 +154,46 @@ const renderAbout = () => {
 
 }
 
+// searching for movies
+
+const fetchSearch = async (nameOfMovie) => { //for fetching the search url
+  const url = constructUrl("search/movie");
+  const res = await fetch(`${url}&query=${nameOfMovie}`);
+  return res.json();
+}
+const SearchMovies = async (nameOfMovie) => {
+  const res = await fetchSearch(nameOfMovie);
+  //if there is no movie under the name that is written in the input field
+  if (res.results.length === 0) {
+    const noMovies = document.querySelector("#noMovies");
+    noMovies.style.display = "block"; //noMovies div is hidden in the html and  we wan to show the message that's why we change display none to display block
+    document.addEventListener("click", () => { //to disappear the message
+      noMovies.style.display = "none";
+    })
+  }
+  //else if we have movies under the name show us 
+  else {
+    CONTAINER.innerHTML = "";
+    renderMovies(res.results);
+  }
+}
+
+const renderSearchMovies = () => {
+  const searchForm = document.querySelector("#form-search");
+  const inputValue = document.querySelector("#search-input");
+  //to give functionality to the close icon
+  document.querySelector(".close").addEventListener("click", () => {
+    return searchForm.reset();
+  })
+  //giving the name of the movie to SearchMovies method
+  return searchForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    SearchMovies(inputValue.value);
+  })
+
+}
+
+//ending of search for movies
 
 // navbar menu for responsiving the navbar
 const navbarMenu = () => {
@@ -170,5 +208,9 @@ const navbarMenu = () => {
   })
 }
 
-document.addEventListener("DOMContentLoaded", autorun(), renderFilterMovies(), renderAbout(), renderHome(), navbarMenu());
+document.addEventListener("DOMContentLoaded", autorun(), renderAbout(), renderSearchMovies(), renderHome(), navbarMenu());
+
+
+
+
 
