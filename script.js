5cb23c3336f8aedda57d48d9dec66300b19d204e
Baraa-Nazar <baraa.nazar.ph@gmail.com>
2022-11-17 13:44:05 +0300

    genres_ids to words

--- script.js
+++ script.js
@@ -12,41 +12,6 @@ window.addEventListener("click", ()=>{
   document.getElementById("render-search").classList.toggle("hidden")
 
 })
-// const genreTranslations ={
-//   28:"Action",
-//   12:"Adventure",
-//   16:"Animation",
-//   35:"Comedy",
-//   80:"Crime",
-//   99:"Documentary",
-//   18:"Drama",
-//   10751:"Family",
-//   14:"Fantasy",
-//   36:"History",
-//   27:"Horror",
-//   10402:"Music",
-//   9648:"Mystery",
-//   10749:"Romance",
-//   878:"Science Fiction",
-//   10770:"TV Movie",
-//   53:"Thriller",
-//   10752:"War",
-//   37:"Western"
-// }
-// console.log(genres)
-// let keys = Object.keys(genres)
-// let values = Object.values(genres)
-// console.log(keys)
-// console.log(values)
-// function convertGenres(key){
-//   if(keys === genres.keys)
-//   return genres.values}
-
-      // for(let i=0; i< genres.length; i++){
-      // let converter = genres[i].id
-      // converter = genres[i].name
-      // return converter
-      // }
 
 //                     -----------------------
 
