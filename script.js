3299a12913d0a689615052fff59f9dc39ed5f8d8
Nina Hawari <nina.hawari@gmail.com>
2022-11-18 20:41:07 +0200

    fixing js

--- script.js
+++ script.js
@@ -8,12 +8,10 @@ function toggleIt() {
 function toggleItNavbar() {
   return openMenu.classList.toggle("hidden");
 }
-
-document.getElementById("movie-search-box").addEventListener("click", ()=>{
+window.addEventListener("click", ()=>{
   document.getElementById("render-search").classList.toggle("hidden")
 
 })
-
 // const genreTranslations = {
 //   28:"Action",
 //   12:"Adventure",
@@ -92,16 +90,6 @@ const movieDetails = async (movie) => {
   const genres = genre.map((g) => { 
     return `<li class="list-none inline-flex justify-between mr-1 text-xs">${g.name}</li>`
   }).join('')
-
-
-  // const renderGenres = async (genre) => {
-  //   const genreRes = await fetchGenres();
-  //   console.log(genre);
-  // }
-  const director = credits.crew.find((item) =>
-  item.job.toLowerCase().includes("Director")
-  );
-
   const directorJob = director.map((dir) => { 
     if (dir.job == "Director") {
       return `<h3 class="inline-block">${dir.name}</h3> ` 
@@ -111,7 +99,6 @@ const movieDetails = async (movie) => {
   // const director = credits.crew.find((item) =>
   //   item.job.toLowerCase().includes("Director")
   // );
-
   const cast = credits.cast
     .slice(0, 5)
     .map((actor) => {
@@ -142,16 +129,11 @@ const actorDetails = async (actor) => {
   const details = await fetchPersonDetails(actor);
   renderActorPage(details);
 };
-
-
 const gridColumns = "grid grid-cols-3 gap-5 container mx-auto";
-
-
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovies = (movies) => {
   movies.map((movie) => {
     const movieDiv = document.createElement("div");
-
     movieDiv.innerHTML = `<div class="transform transition duration-500 hover:scale-95">
     <img id="poster" class="moviePoster cursor-pointer rounded-sm" src="${
           BACKDROP_BASE_URL + movie.poster_path
@@ -167,35 +149,37 @@ const renderMovies = (movies) => {
       BACKDROP_BASE_URL + movie.backdrop_path
     }" alt="${movie.title} poster">`;
     CONTAINER.appendChild(movieDiv);
-   
     // SWIPER.appendChild(backdropDiv);
   });
-
-   CONTAINER.setAttribute('class',gridColumns);
-
+  CONTAINER.setAttribute('class',gridColumns);
 };
 
 const noGrid = "container mx-auto"
 
-console.log(CONTAINER) 
 // You'll need to play with this function in order to add features and enhance the style.
 const renderMovie = (movieDetails) => {
-  const { details, cast,companies, genres,official ={} } = movieDetails;
-  const {poster_path,title,release_date,runtime,overview,vote_average,vote_count,original_language,backdrop_path} = details;
+  const {details, cast,companies,director, genres,official ={} } = movieDetails;
+  const {poster_path,title,release_date,runtime,overview,vote_average,vote_count,original_language} = details;
   CONTAINER.innerHTML = "";
-  CONTAINER.innerHTML = `
-    <div class="w-100 flex flex-col gap-2">
-    <div class="flex justify-center"><iframe width="1400" height="800"src="https://www.youtube.com/embed/${
-      official.key
-    }" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe></div>
+  CONTAINER.innerHTML = `<header
+  class="relative flex items-center justify-center h-screen mb-12 overflow-hidden"
+>
+  <div
+    class="relative z-30 p-5 text-5xl font-gotham font-bold text-white bg-opacity-50 rounded-xl flex justify-start"
+  >${title}</div>
+  <video autoplay loop muted class="absolute z-10 w-auto min-w-full min-h-full max-w-none">
+    <source src="https://www.youtube.com/embed/${official.key}" type="video/mp4"/>
+    Your browser does not support the video tag.
+  </video>
+</header>
+    <div class="None">
+    <iframe class="w-auto min-w-full min-h-full max-w-none" width="1060" height="1015" src="https://www.youtube.com/embed/${official.key}" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
         
     <div class="flex flex-col gap-2"><div class="w-2/4">
-
              <img id="movie-backdrop class="cursor-pointer" src=${
                BACKDROP_BASE_URL + poster_path
              }>
         </div>
-
   
         <div class=" text-white w-4/5 font-gotham">
           <h2 id="movie-title class="text-white font-gotham text-2xl">${title}</h2>
@@ -292,7 +276,6 @@ return res.json();
 }
 
 
-
 function genresList(genres) {
   // const genres = await fetchGenres();
   genres.forEach((g) => { 
@@ -307,7 +290,6 @@ const genreDiv = document.createElement("div");
 const fetchMoviesByGenre = (genreId) => {
 
 }
-
 
 function searchShow(query){
   const search_URL = `https://api.themoviedb.org/3/search/movie?api_key=473329bca30a210d04b15f4cda32a5e7&language=en-US&query=${query}&page=1&include_adult=false`
@@ -358,12 +340,12 @@ function renderResults(results){
     })
     const container = document.createElement("div")
     container.innerHTML = `
-    <div class="flex w-full dark:bg-gray-900 bg-slate-100 hover:bg-black hover:text-white hover:translate-x-2 hover:transition hover:text-bold hover:uppercase">
-    <img class=" h-20 w-18" src="${BACKDROP_BASE_URL}${result.backdrop_path}">
-    <ul class="flex w-full flex-col dark:text-white font-sans p-2">
+    <div class="flex w-full z-0">
+    <img class=" h-16 w-16" src="${BACKDROP_BASE_URL}${result.backdrop_path}">
+    <ul class="flex w-full flex-col">
     <li><span class=" flex flex-1 items-center">${result.original_title}</span></li>
     <li><span class=""><span style="font-size:100%;color:gold;">&starf;</span> ${result.vote_average}</span></li>
-    <li><p> ${genresConverter}</p></li>
+    <li><div class="flex"><p>${genresConverter}</p> </div></li>
     </ul>
     </div>
     `
